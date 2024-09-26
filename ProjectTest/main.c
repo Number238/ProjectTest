ec589b5cedd3b985320796374c61555a6794aa41
Number238 <Andy.ji2000@outlook.com>
2024-09-26 13:39:45 +0100

    20240926Trial

--- ProjectTest/main.c
+++ ProjectTest/main.c
@@ -8,6 +8,18 @@
 #include "memory_protection.h"
 #include <main.h>
 
+#include "leds.h"
+#include "spi_comm.h"
+
+#include "sensors/proximity.h"
+
+#include "epuck1x/uart/e_uart_char.h"
+#include "stdio.h"
+#include "serial_comm.h"
+
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
 
 int main(void)
 {
@@ -16,11 +28,53 @@ int main(void)
     chSysInit();
     mpu_init();
 
+    // Your initialisations here
+    clear_leds();
+    spi_comm_start();
+    motors_init();
+    messagebus_init(&bus, &bus_lock, &bus_condvar);
+    proximity_start(1);
+    calibrate_ir();
+    serial_start();
+
+   	char str[100];
+   	int str_length;
+
+	//waits 1 second
+	set_body_led(1);
+	chThdSleepMilliseconds(1000);
+	set_led(LED1 , 1);
+    chThdSleepMilliseconds(1000);
+	  set_led(LED3 , 0);
+    chThdSleepMilliseconds(1000);
+    set_body_led(0);
+    chThdSleepMilliseconds(1000);
+	  set_led(LED1 , 0);
+  chThdSleepMilliseconds(1000);
+	  set_led(LED3 , 1);
+  chThdSleepMilliseconds(1000);
+
 
     /* Infinite loop. */
     while (1) {
-    	//waits 1 second
+
+      if (get_calibrated_prox(0)>=200 || get_calibrated_prox(7)>=200)
+    		  {
+    	 set_body_led(0);
+   	  left_motor_set_speed(500);
+   	  right_motor_set_speed(-500);
+    		  }
+      else
+    	  {
+    	  set_body_led(1);
+       	  left_motor_set_speed(0);
+       	  right_motor_set_speed(0);
+       	str_length = sprintf(str, "Hello World\n");
         chThdSleepMilliseconds(1000);
+       	e_send_uart1_char(str, str_length);
+    	  }
+
+
     }
 }
 
